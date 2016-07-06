6b0912425ab02ad8ab2801332ce9ac027bf02981
Roy'el Byrd <rbyrd@DHF-MBA-44.local>
2016-07-06 14:27:27 -0400

    This the final project(1st commit)

---
+++ script.js
@@ -0,0 +1,5 @@
+var FirstAlert= setInterval(Alert, 3000);
+
+function Alert(){
+    alert('Welcome! This website is now yours to customize. All the instructions are in the first paragraph. Have fun!')
+}
