a495ff87560862bbac8bf5bd2fe7ceca1699d3c9
Royboy535 <royelbyrd535@gmail.com>
2016-07-07 16:01:42 -0400

    Add files via upload

---
+++ Final-Project-master/script.js
@@ -0,0 +1,19 @@
+/*var FirstAlert= setTimeout(function(){
+    alert('Welcome! This website is now yours to customize. All the instructions are in the first paragraph. Have fun!');
+}, 3000);
+*/
+
+var orgintxt= document.getElementById('origintxt1');
+var myB = document.getElementById('Button');
+
+//myB.onclick = function changeText2(){};//
+
+function changeText2(){
+    document.getElementById('input-text');
+    document.getElementById('origintxt1').innerHTML = userInput;
+    
+}
+
+var txt = 'hello';
+
+myB.onclick(function(){alert('hello')});
