466248fe1012a0cfdbdd990829b2db377260e56c
Royboy535 <royelbyrd535@gmail.com>
2016-07-08 19:56:51 -0400

    Add files via upload
    This is the almost fully functional website. I just need the text replacement code from Adam.

--- script.js
+++ script.js
@@ -1,5 +1,54 @@
-var FirstAlert= setInterval(Alert, 3000);
+//importing text replacement button, text, and input
+var originalText = document.getElementById('Text');
+var newText = document.getElementsByName('Replacement Text');
+var myButton = document.getElementById('Button');
+//importing background color
+var bgRed = document.getElementById('colorRed');
+var bgTan = document.getElementById('colorTan');
+var bgBlue = document.getElementById('colorBlue');
+var bgYellow = document.getElementById('colorYellow');
+//importing my text buttons
+var textBlack = document.getElementById('textBlack');
+var textRed = document.getElementById('textRed');
+var textYellow = document.getElementById('textYellow');
+var textBlue = document.getElementById('textBlue');
 
-function Alert(){
-    alert('Welcome! This website is now yours to customize. All the instructions are in the first paragraph. Have fun!')
-}
+//changes the background color
+
+bgBlue.addEventListener("click", function () {
+    document.body.style.backgroundColor = "blue"; 
+   
+});
+
+bgTan.addEventListener("click", function () {
+    document.body.style.backgroundColor = "tan"; 
+    
+},false);
+
+bgYellow.addEventListener("click", function () {
+    document.body.style.backgroundColor = "yellow"; 
+    
+});
+
+bgRed.addEventListener("click", function () {
+    document.body.style.backgroundColor = "red"; 
+    
+},false);
+
+textBlack.addEventListener("click",function () {
+document.getElementById('Text').style.color = "black"
+    },false);
+    
+    
+ textRed.addEventListener("click",function () {
+   document.getElementById('Text').style.color = "red";
+    },false);
+
+  textBlue.addEventListener("click",function () {
+   document.getElementById('Text').style.color = "blue";
+    },false);
+  
+ textYellow.addEventListener("click",function () {
+   document.getElementById('Text').style.color = "yellow";
+    },false);
+  
